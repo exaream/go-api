c35aa053f82d00eebe8fea2a03956e7cd68b1a8a
exaream <emctwi8@gmail.com>
2024-02-09 00:04:36 +0900

    Add logging middleware

---
+++ api/middlewares/logging.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+type responseLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResponseLoggingWriter(w http.ResponseWriter) *responseLoggingWriter {
+	return &responseLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rlw *responseLoggingWriter) WriteHeader(code int) {
+	rlw.code = code
+	rlw.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(next func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.RequestURI)
+		rlw := NewResponseLoggingWriter(w)
+		next(rlw, r)
+		log.Println("status_code", rlw.code)
+	}
+}
