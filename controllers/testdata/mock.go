b8fe9201d91846836a9b2e0176a6ebb5c6128a8d
exaream <emctwi8@gmail.com>
2024-02-18 22:31:56 +0900

    Add tests for article controller

---
+++ controllers/testdata/mock.go
@@ -0,0 +1,29 @@
+package testdata
+
+import "github.com/exaream/go-api/models"
+
+type serviceMock struct{}
+
+func NewServiceMock() *serviceMock {
+	return &serviceMock{}
+}
+
+func (s *serviceMock) ListArticle(page int) ([]*models.Article, error) {
+	return ArticleList, nil
+}
+
+func (s *serviceMock) GetArticle(id int) (*models.Article, error) {
+	return ArticleList[0], nil
+}
+
+func (s *serviceMock) PostArticle(article *models.Article) (*models.Article, error) {
+	return ArticleList[0], nil
+}
+
+func (s *serviceMock) PostNice(article *models.Article) (*models.Article, error) {
+	return ArticleList[0], nil
+}
+
+func (s *serviceMock) PostComment(comment *models.Comment) (*models.Comment, error) {
+	return CommentList[0], nil
+}
