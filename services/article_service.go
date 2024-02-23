eeb3c9e00e70fc9a515f28389c4aaefd95d27b58
exaream <emctwi8@gmail.com>
2024-02-23 15:29:31 +0900

    Use chan for article service

--- services/article_service.go
+++ services/article_service.go
@@ -3,7 +3,6 @@ package services
 import (
 	"database/sql"
 	"errors"
-	"sync"
 
 	"github.com/exaream/go-api/apperrors"
 	"github.com/exaream/go-api/models"
@@ -24,31 +23,44 @@ func (s *AppService) ListArticle(page int) ([]*models.Article, error) {
 }
 
 func (s *AppService) GetArticle(articleID int) (*models.Article, error) {
-	var (
-		article                    *models.Article
-		commentList                []*models.Comment
-		articleErr, commentErr     error
-		articleMutex, commentMutex sync.Mutex
-		wg                         sync.WaitGroup
-	)
+	type articleRes struct {
+		article *models.Article
+		err     error
+	}
 
-	wg.Add(2)
+	var article *models.Article
+	var articleErr error
+	articleChan := make(chan articleRes)
+	defer close(articleChan)
 
-	go func(db *sql.DB, articleID int) {
-		defer wg.Done()
-		articleMutex.Lock()
-		article, articleErr = repositories.SelectArticleDetail(db, articleID)
-		articleMutex.Unlock()
-	}(s.db, articleID)
+	type commentRes struct {
+		commentList []*models.Comment
+		err         error
+	}
 
-	go func(db *sql.DB, articleID int) {
-		defer wg.Done()
-		commentMutex.Lock()
-		commentList, commentErr = repositories.SelectCommentList(db, articleID)
-		commentMutex.Unlock()
-	}(s.db, articleID)
+	var commentList []*models.Comment
+	var commentErr error
+	commentChan := make(chan commentRes)
+	defer close(commentChan)
 
-	wg.Wait()
+	go func(ch chan<- articleRes, db *sql.DB, articleID int) {
+		article, err := repositories.SelectArticleDetail(db, articleID)
+		ch <- articleRes{article, err}
+	}(articleChan, s.db, articleID)
+
+	go func(ch chan<- commentRes, db *sql.DB, articleID int) {
+		commentList, err := repositories.SelectCommentList(db, articleID)
+		ch <- commentRes{commentList, err}
+	}(commentChan, s.db, articleID)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case res := <-articleChan:
+			article, articleErr = res.article, res.err
+		case res := <-commentChan:
+			commentList, commentErr = res.commentList, res.err
+		}
+	}
 
 	if articleErr != nil {
 		if errors.Is(articleErr, sql.ErrNoRows) {
